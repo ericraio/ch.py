6bb10c9254b28c29498861977e9ac559c18a0303
lumirayz <lumirayz@gmail.com>
2011-02-11 17:29:36 +0100

    tweaking session handling

--- ch.py
+++ ch.py
@@ -589,6 +589,9 @@ class RoomConnection:
 	def rcmd_g_participants(self, args):
 		args = ":".join(args)
 		args = args.split(";")
+		for user in self._userlist:
+			user.clearSessionIds(self)
+		self._userlist = list()
 		for data in args:
 			data = data.split(":")
 			name = data[3].lower()
@@ -597,7 +600,7 @@ class RoomConnection:
 				name = name,
 				room = self
 			)
-			user._sids.add(data[0])
+			user.addSessionId(self, data[0])
 			self._userlist.append(user)
 	
 	def rcmd_participant(self, args):
@@ -605,7 +608,7 @@ class RoomConnection:
 			name = args[3].lower()
 			if name == "none": return
 			user = User(name)
-			user._sids.remove(args[1])
+			user.removeSessionId(self, args[1])
 			self._userlist.remove(user)
 			if user not in self._userlist or not self._userlistEventUnique:
 				self.onLeave(user)
@@ -616,7 +619,7 @@ class RoomConnection:
 				name = name,
 				room = self
 			)
-			user._sids.add(args[1])
+			user.addSessionId(self, args[1])
 			if user not in self._userlist: doEvent = True
 			else: doEvent = False
 			self._userlist.append(user)
@@ -1094,7 +1097,7 @@ class _User:
 	####
 	def __init__(self, name, **kw):
 		self._name = name.lower()
-		self._sids = set()
+		self._sids = dict()
 		self._msgs = list()
 		for attr, val in kw.items():
 			setattr(self, "_" + attr, val)
@@ -1103,12 +1106,51 @@ class _User:
 	# Properties
 	####
 	def getName(self): return self._name
-	def getSessionIds(self): return self._sids
+	def getSessionIds(self, room = None):
+		if room:
+			return self._sids.get(room, set())
+		else:
+			return set.union(*self._sids.values())
+	def getRooms(self): return self._sids.keys()
+	def getRoomNames(self): return [room.room for room in self.getRooms()]
 	def getUnid(self): return self.getLastMessage().unid
 	
 	name = property(getName)
 	sessionids = property(getSessionIds)
 	unid = property(getUnid)
+	rooms = property(getRooms)
+	roomnames = property(getRoomNames)
+	
+	####
+	# Util
+	####
+	def addSessionId(self, room, sid):
+		if room not in self._sids:
+			self._sids[room] = set()
+		self._sids[room].add(sid)
+	
+	def removeSessionId(self, room, sid):
+		try:
+			self._sids[room].remove(sid)
+			if len(self._sids[room]) == 0:
+				del self._sids[room]
+		except KeyError:
+			pass
+	
+	def clearSessionIds(self, room):
+		try:
+			del self._sids[room]
+		except KeyError:
+			pass
+	
+	def hasSessionId(self, room, sid):
+		try:
+			if sid in self._sids[room]:
+				return True
+			else:
+				return False
+		except KeyError:
+			return False
 
 ################################################################
 # Message class
